bfea0a288eec85c7e9e55bd6ee0d4bef0d9bc209
Lou Garramone <lou@lg4techsolutions.com>
2021-04-13 13:02:25 -0400

    In Progress - no support for other deployment methods.

--- Serverless/StaticWebAppsAzureDevOps.sh
+++ Serverless/StaticWebAppsAzureDevOps.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
  
-rg=myStaticWebApp
-location=eastus
+rg=mystaticwebapp
+location=eastus2
 
 # Create Resource Group
 az group create --name $rg --location $location
 
+# Clone files from repo
+git clone https://lg4tech@dev.azure.com/lg4tech/sample-webapp/_git/sample-webapp
+cd sample-webapp
+
 # Create Static Web App
 az staticwebapp create \
-	--name $rg \
-	--group $rg \
-	--location $location \ 
+	-n mystaticwebapp \
+	-g mystaticwebapp \
+	-l eastus2 \
+	-s https://lg4tech@dev.azure.com/lg4tech/sample-webapp/_git/sample-webapp \
+	-b master \
+	--token <TOKEN>
